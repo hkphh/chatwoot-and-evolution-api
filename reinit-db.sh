262e8b53c70e8e4c9d2ca31788b10a2b4b0509d5
root <root@wachat.home>
2026-06-10 13:16:13 +0000

    new

---
+++ reinit-db.sh
@@ -0,0 +1,11 @@
+#Remove folders
+rm -rf ./database/chatwoot/redis
+rm -rf ./database/chatwoot/postgres
+rm -rf ./database/evolution/redis
+rm -rf ./database/evolution/postgres
+
+#Create folders
+mkdir ./database/chatwoot/redis
+mkdir ./database/chatwoot/postgres
+mkdir ./database/evolution/redis
+mkdir ./database/evolution/postgres
